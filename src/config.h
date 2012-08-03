56b42a41e93f9baa14bf0c6f034c35d1b335615a
Michael J. Beer <michael.josef.beer@googlemail.com>
2012-08-04 00:46:24 +0200

    MIB: UPDATE - Small format corrections in license comments.

--- src/config.h
+++ src/config.h
@@ -1,4 +1,4 @@
-/**
+/*
  * (C) 2010, 2012 Michael J. Beer
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -9,7 +9,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
--- src/error.h
+++ src/error.h
@@ -1,16 +1,20 @@
-/**2010 * (C) 2010 Michael J. Beer * This program is free software; you can
- * redistribute it and/or modify * it under the terms of the GNU General Public
- * License as published by * the Free Software Foundation; either version 3 of
- * the License, or * (at your option) any later version.2010 *2010 * This
- * program is distributed in the hope that it will be useful,2010 * but WITHOUT
- * ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the * GNU General Public License for
- * more details.2010 2010 * You should have received a copy of the GNU General
- * Public License * along with this program; if not, write to the Free Software
- * * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.2010
+/*
+ * (C) 2012 Michael J. Beer
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
+ 
 
 #ifndef __ERROR_H__
 #define __ERROR_H__
