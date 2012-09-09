722386082b2f8a53b5c51433e69e17384e8d0c3a
Michael Josef Beer <michael.josef.beer@googlemail.com>
2012-09-09 02:24:24 +0200

    MIB: UPDATE - Refactored expression.h

--- src/expression.h
+++ src/expression.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2010 Michael J. Beer
+ * (C) 2010, 2012 Michael J. Beer
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -17,7 +17,7 @@
 
 /**
  * Provides functions for dealing with expressions.
- * This includes basic funcitons for
+ * This peovides basic functions for
  * Creation
  * Disposal
  * Access
@@ -34,7 +34,46 @@
 
 
 
+/*******************************************************************************
+  E X R E S S I O N   S T R U C T U R E
+ *******************************************************************************/
+
+
+/**
+ * Represents a lisp expression.
+ * Actual implementation is provided within included header, see the very bottom
+ * of this file.
+ */
 struct Expression;
+
+
+typedef struct Expression *(NativeFunction)(struct Expression *env, struct Expression *);
+
+
+
+/*******************************************************************************
+                              CONSTANT EXPRESSIONS
+ *******************************************************************************/
+
+
+
+/** 
+ * The NIL atom
+ */
+#define NIL                            __NIL
+
+
+/**
+ * The T atom
+ */
+#define T                              __T
+
+
+/**
+ * The symbol that denotes the variable to store abundand arguments of lambda
+ * calls
+ */
+#define REST                           __REST
 
 
 
@@ -43,10 +82,11 @@ struct Expression;
  *******************************************************************************/
 
 
+
 /**
  * Gives the type of the expression
  */
-#define EXPRESSION_TYPE(x) (x->type & EXPR_TYPE_AREAL)
+#define EXPRESSION_TYPE(x)             __EXPRESSION_TYPE(x)
 
 
 /** 
@@ -60,22 +100,25 @@ struct Expression;
  * @param x a pointer to an expression
  * @param y unsigned integer indicating the type
  */
-#define EXPR_OF_TYPE(x, y) (x && EXPRESSION_TYPE(x) == y)
+#define EXPR_OF_TYPE(x, y)             __EXPR_OF_TYPE(x, y)
+
 
 /**
  * Checks whether x is a pointer type 
  */
-#define EXPR_IS_POINTER(x) (x->type & EXPR_POINTER)
+#define EXPR_IS_POINTER(x)             __EXPR_IS_POINTER(x)
+
 
 /**
  * Checks whether x is a Cons cell
  */
-#define EXPR_IS_CONS(x) EXPR_OF_TYPE(x, EXPR_CONS)
+#define EXPR_IS_CONS(x)                EXPR_OF_TYPE(x, EXPR_CONS)
+
 
 /** 
  * Checks whether x is NIL
  */
-#define EXPR_IS_NIL(x) (x == NIL)
+#define EXPR_IS_NIL(x)                 __EXPR_IS_NIL(x)
 
 
 /**
@@ -84,15 +127,13 @@ struct Expression;
 #define EXPR_IS_VALID(x) (x && \
         !EXPR_OF_TYPE(x, EXPR_NO_TYPE))
 
-
 /** 
  * Sets the type of the expression 
  * @param expr pointer to expression
  * @param type the new type
  */
-#define EXPR_SET_TYPE(expr, type) (expr->type = (\
-            (expr->type & ~EXPR_TYPE_AREAL) | \
-            (type & EXPR_TYPE_AREAL)))
+#define EXPR_SET_TYPE(expr, type)      __EXPR_SET_TYPE(expr, type)
+
 
 /*
    Internally, every expression keeps a type - field
@@ -101,139 +142,96 @@ struct Expression;
    Bit 6 indicates no pointer to be freed (an "atomic type")
    */
 
-
 /** 
  * Defines the bytes to be used for determining the type
  */
-#define EXPR_TYPE_AREAL ((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | \
-        (1 << 3) | (1 << 2) | 2 | 1)
+#define EXPR_TYPE_AREAL                          \
+    ((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | \
+     (1 << 3) | (1 << 2) | 2 | 1)
 
 
 /**
  * This represents just an uninitialised expression
  */
-#define EXPR_NO_TYPE 0
+#define EXPR_NO_TYPE                   0
+
+
 
 /**
  * Expression contains a pointer (to be freed)
  */
-#define EXPR_POINTER (1 << 7)
+#define EXPR_POINTER                   (1 << 7)
+
 
 /**
  * Expression contains a an atomar value  (not to be freed)
  */
-#define EXPR_ATOM (1 << 6)
+#define EXPR_ATOM                      (1 << 6)
 
 
 /**
  * Expression is an integer
  */
-#define EXPR_INTEGER (EXPR_ATOM | 1)
+#define EXPR_INTEGER                   (EXPR_ATOM | 1)
+
 
 /**
  * Expression is a float
  */
-#define EXPR_FLOAT (EXPR_ATOM | 2)
+#define EXPR_FLOAT                     (EXPR_ATOM | 2)
+
 
 /**
  * Expression is an character
  */
-#define EXPR_CHARACTER (EXPR_ATOM | 3)
+#define EXPR_CHARACTER                 (EXPR_ATOM | 3)
+
 
 /**
  * Expression is an native function
  */
-#define EXPR_NATIVE_FUNC (EXPR_ATOM | 4)
+#define EXPR_NATIVE_FUNC               (EXPR_ATOM | 4)
+
 
 /**
  * Expression is an string
  */
-#define EXPR_STRING (EXPR_POINTER | 1)
+#define EXPR_STRING                    (EXPR_POINTER | 1)
+
 
 /**
  * Expression is asymbol
  */
-#define EXPR_SYMBOL (EXPR_POINTER | 2)
+#define EXPR_SYMBOL                    (EXPR_POINTER | 2)
+
 
 /**
  * Expression is an cons cell
  */
-#define EXPR_CONS (EXPR_POINTER | 3)
+#define EXPR_CONS                      (EXPR_POINTER | 3)
+
 
 /**
  * Expression is a function
  */
-#define EXPR_FUNCTION (EXPR_POINTER | 5)
+#define EXPR_FUNCTION                  (EXPR_POINTER | 5)
+
 
 /**
  * Expression is an hash table
  */
-#define EXPR_ENVIRONMENT (EXPR_POINTER | 6)
+#define EXPR_ENVIRONMENT               (EXPR_POINTER | 6)
+
 
 /**
  * Expression is an hash table
  */
-#define EXPR_LAMBDA (EXPR_POINTER | 7)
-
-
-/*******************************************************************************
-                              CONSTANT EXPRESSIONS
- *******************************************************************************/
-
-
-extern struct Expression expressionNil;
-/** 
- * The NIL atom
- */
-#define NIL (&expressionNil)
-
-extern struct Expression expressionT ;
-/**
- * The T atom
- */
-#define T (&expressionT)
-
-extern struct Expression expressionRest;
-/**
- * The symbol that denotes the variable to store abundand arguments of lambda
- * calls
- */
-#define REST (&expressionRest)
-
-
-/*******************************************************************************
-  E X R E S S I O N   S T R U C T U R E
- *******************************************************************************/
-
-
-
-typedef struct Expression *(NativeFunction)(struct Expression *env, struct Expression *);
-
-
-struct Cons {
-    struct Expression *car, *cdr;
-};
-
-
-struct Expression {
-    union {
-        char *string;
-        int integer;
-        char character;
-        double floating;
-        struct Cons *cons;
-        struct Expression *(*nativeFunc)(struct Expression *env, struct Expression *);
-        struct Environment *env;
-        struct Lambda *lambda;
-    } data;
-    unsigned int counter;
-    unsigned char type;
-};
+#define EXPR_LAMBDA                    (EXPR_POINTER | 7)
 
 
 
 /*******************************************************************************
-  C R E A T I O N   &   D I S P O S A L
+                     C R E A T I O N   &   D I S P O S A L
  *******************************************************************************/
 
 
@@ -286,98 +284,109 @@ struct Expression *createEnvironmentExpression(struct Environment *env);
 /** 
  * Creates an invalid expression
  */
-#define CREATE_INVALID_EXPRESSION(env) expressionCreate(env, EXPR_NO_TYPE, 0)
+#define CREATE_INVALID_EXPRESSION(env)            \
+    expressionCreate(env, EXPR_NO_TYPE, 0) 
 
 
 /**
  * Creates a Symbol out of the string str
  */
-#define CREATE_SYMBOL(env, str) createSymbol(env, str)
+#define CREATE_SYMBOL(env, str)        createSymbol(env, str)
 
 
 /**
  * Creates a float expression
  */
-#define CREATE_FLOAT_EXPRESSION(env, x) expressionCreate(env, EXPR_FLOAT, (void *)&x)
+#define CREATE_FLOAT_EXPRESSION(env, x)           \
+    expressionCreate(env, EXPR_FLOAT, (void *)&x)
 
 
 /**
  * Creates a string expression
  */
-#define CREATE_STRING_EXPRESSION(env, x) expressionCreateString(env, x)
+#define CREATE_STRING_EXPRESSION(env, x)          \
+    expressionCreateString(env, x)
 
 
 /**
  * Creates a int expression
  */
-#define CREATE_INT_EXPRESSION(env, x) expressionCreate(env, EXPR_INTEGER, (void *)&x)
+#define CREATE_INT_EXPRESSION(env, x)               \
+    expressionCreate(env, EXPR_INTEGER, (void *)&x)
+
 
 /**
  * Creates a char expression
  */
-#define CREATE_CHAR_EXPRESSION(env, x) expressionCreate(env, EXPR_CHARACTER, (void *)&x)
+#define CREATE_CHAR_EXPRESSION(env, x)                \
+    expressionCreate(env, EXPR_CHARACTER, (void *)&x)
 
 
 
 /******************************************************************************
-  A C C E S S
+                                  A C C E S S
  ******************************************************************************/
+
+
 
 /**
  * Get integer value of expression
  * @param expr pointer to an expression
  */
-#define EXPRESSION_INTEGER(expr) (expr->data.integer)
+#define EXPRESSION_INTEGER(expr)       __EXPRESSION_INTEGER(expr)
 
 
 /**
  * Get floating value of expression
  * @param expr pointer to an expression
  */
-#define EXPRESSION_FLOATING(expr) (expr->data.floating)
+#define EXPRESSION_FLOATING(expr)      __EXPRESSION_FLOATING(expr)
 
 
 /**
  * Get character value of expression
  * @param expr pointer to an expression
  */
-#define EXPRESSION_CHARACTER(expr) (expr->data.character)
+#define EXPRESSION_CHARACTER(expr)     __EXPRESSION_CHARACTER(expr)
 
 
 /**
  * Get string value of expression
  * @param expr pointer to an expression
  */
-#define EXPRESSION_STRING(expr) (expr->data.string)
+#define EXPRESSION_STRING(expr)        __EXPRESSION_STRING(expr)
 
 
 /**
  * Get the pointer to native function
  * @param expr pointer to an expression
  */
-#define EXPRESSION_NATIVE_FUNC(expr) ((expr)->data.nativeFunc)
+#define EXPRESSION_NATIVE_FUNC(expr)   __EXPRESSION_NATIVE_FUNC(expr)
+
 
 /**
   * Get the pointer to an environment
   * @param expr pointer to an expression
   */
-#define EXPRESSION_ENVIRONMENT(expr) ((expr)->data.env)
+#define EXPRESSION_ENVIRONMENT(expr)   __EXPRESSION_ENVIRONMENT(expr)
+
 
 /**
  * Get the pointer to a lambda form
  */
-#define EXPRESSION_LAMBDA(expr) ((expr)->data.lambda)
+#define EXPRESSION_LAMBDA(expr)        __EXPRESSION_LAMBDA(expr)
+
 
 /**
  * Get the car of a cons cell
  */
-#define EXPRESSION_CAR(expr) ((expr)->data.cons->car)
+#define EXPRESSION_CAR(expr)           __EXPRESSION_CAR(expr)
 
 
 /**
  * Get the cdr of a cons cell 
  */
-#define EXPRESSION_CDR(expr) ((expr)->data.cons->cdr)
+#define EXPRESSION_CDR(expr)           __EXPRESSION_CDR(expr)
 
 
 /** 
@@ -394,7 +403,7 @@ struct Expression *expressionAssign(struct Expression *env, struct Expression *e
  * @param expr cons cell whose car should be set 
  * @param expr2 any expression that should be set as the car of the cons cell
  */
-#define EXPRESSION_SET_CAR(expr, expr2) (expr)->data.cons->car = expr2;
+#define EXPRESSION_SET_CAR(expr, expr2)          __EXPRESSION_SET_CAR(expr, expr2)
 
 
 /**
@@ -402,12 +411,12 @@ struct Expression *expressionAssign(struct Expression *env, struct Expression *e
  * @param expr cons cell whose cdr should be set 
  * @param expr2 any expression that should be set as the cdr of the cons cell
  */
-#define EXPRESSION_SET_CDR(expr, expr2) (expr)->data.cons->cdr = expr2;
+#define EXPRESSION_SET_CDR(expr, expr2)          __EXPRESSION_SET_CDR(expr, expr2)
 
 
 
 /*******************************************************************************
- * CASTS
+                                     CASTS
  *******************************************************************************/
 
 
@@ -417,15 +426,15 @@ struct Expression *expressionAssign(struct Expression *env, struct Expression *e
  * Returns 0 in case of failure
  * TODO: Report error!
  */
-#define EXPR_TO_FLOAT(expr) \
-    ((EXPR_OF_TYPE(expr, EXPR_INTEGER)) ?  \
-     (float) EXPRESSION_INTEGER(expr) : \
+#define EXPR_TO_FLOAT(expr)                                             \
+    ((EXPR_OF_TYPE(expr, EXPR_INTEGER)) ?                               \
+     (float) EXPRESSION_INTEGER(expr) :                                 \
      (EXPR_OF_TYPE(expr, EXPR_FLOAT)) ? EXPRESSION_FLOATING(expr) : 0)
 
 
 
 /*******************************************************************************
-  I N T E R N A L S
+                               I N T E R N A L S
  *******************************************************************************/
 
 
@@ -444,7 +453,29 @@ struct Expression *expressionCreateString(struct Expression *env, char *str);
 struct Expression *createSymbol(struct Expression *env, char *str);
 
 
-#define EXPRESSION_CONS(expr) ((expr)->data.cons)
+#define EXPRESSION_CONS(expr)          __EXPRESSION_CONS(expr)
+
+
+
+/*****************************************************************************
+                         INCLUDE ACTUAL IMPLEMENTATION
+ *****************************************************************************/
+
+
+
+#ifdef USE_PACKED_EXPRESSION_FORMAT
+
+#   include "__expression_packed.h"
+
+#elif USE_EXPANDED_EXPRESSION_FORMAT 
+
+#   include "__expression_expanded.h"
+
+#else   /* expression format - default case */
+
+#   include "__expression_expanded.h"
+
+#endif  /* expression format */
 
 
 
