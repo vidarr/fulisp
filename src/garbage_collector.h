ec5eee2dbda864fb9a2beea04229babe7706f5ce
Michael J. Beer <michael.josef.beer@googlemail.com>
2014-12-21 12:23:20 +0000

    UPDATE - Prepared for usage of alternate garbage collector;

---
+++ src/garbage_collector.h
@@ -0,0 +1,112 @@
+/*
+ * (C) 2014 Michael J. Beer
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __GARBAGE_COLLECTOR_H__
+#define __GARBAGE_COLLECTOR_H__
+
+#include "config.h"
+
+/**
+ * struct in the environment reserved for the GC to store whatever data it 
+ * requires. Actual appearance depends on GC used.
+ */
+#define GC_DEFINE_ENV_INFO __GC_DEFINE_ENV_INFO
+
+/**
+ * struct in each expression reserved for the GC to store whatever data it 
+ * requires. Actual appearance depends on GC used.
+ */
+#define GC_DEFINE_EXPR_INFO __GC_DEFINE_EXPR_INFO
+
+#define GC_INIT_EXPR_INFO(initcounter)   __GC_INIT_EXPR_INFO(initcounter)
+
+/**
+ * Invoke the garbage collector
+ */
+#define GC_RUN(env) __GC_RUN(env)
+
+/**
+ * Initialize ref counter of expr
+ */
+#define GC_INIT_REF_COUNT(env, expr) __GC_INIT_REF_COUNT(env, expr)
+
+/**
+ * Return current ref counter of expr 
+ */
+#define GC_GET_REF_COUNT(env, expr) __GC_GET_REF_COUNT(env, expr)
+
+/**
+ * Decrease ref counter of expr
+ */
+#define GC_DEC_REF_COUNT(env, expr) __GC_DEC_REF_COUNT(env, expr)
+
+/**
+ * Increase ref counter of expr
+ */
+#define GC_INC_REF_COUNT(env, expr) __GC_INC_REF_COUNT(env, expr)
+
+
+/******************************************************************************
+ * Implementation details
+ ******************************************************************************/
+
+
+#if ! defined (GARBAGE_COLLECTOR)
+
+#    error("GARBAGE_COLLECTOR has not been defined!")
+
+# elif      GARBAGE_COLLECTOR == GC_REFERENCE_COUNTING
+
+struct ExprGcInfo {
+    unsigned int counter;
+};
+
+#    define __GC_DEFINE_ENV_INFO
+#    define __GC_DEFINE_EXPR_INFO          struct ExprGcInfo gcInfo;
+#    define __GC_INIT_EXPR_INFO(initcounter) ,{initcounter}
+#    define __GC_RUN(env)                  ERR_OK
+#    define __GC_INIT_REF_COUNT(env, expr) ((expr)->gcInfo.counter = 0)
+#    define __GC_GET_REF_COUNT(env, expr)  ((expr)->gcInfo.counter)
+#    define __GC_DEC_REF_COUNT(env, expr)  ((expr)->gcInfo.counter--)
+#    define __GC_INC_REF_COUNT(env, expr)  ((expr)->gcInfo.counter++)
+
+#elif       GARBAGE_COLLECTOR == GC_MARK_AND_SWEEP
+
+struct GcInfo {
+    int marker;     /* Current marker used */
+};
+
+#    define __GC_DEFINE_ENV_INFO             struct EnvGcInfo gcInfo;
+#    define __GC_DEFINE_EXPR_INFO 
+#    define __GC_INIT_EXPR_INFO(initcounter)
+#    define __GC_RUN(env)                    gc_mark_and_sweep(env)
+#    define __GC_INIT_REF_COUNT(env, expr)   while(0) {};
+#    define __GC_GET_REF_COUNT(env, expr)    1
+#    define __GC_DEC_REF_COUNT(env, expr)    1
+#    define __GC_INC_REF_COUNT(env, expr)    1
+
+int gc_mark_and_sweep(struct Environment *env);
+
+#else
+
+#   error("GARBAGE_COLLECTOR not set!")
+
+#endif
+
+
+
+#endif
