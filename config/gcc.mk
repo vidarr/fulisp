4b7c0a35c777fa5d06ebff40d09dd1ce6cef9509
Michael J. Beer <michael.josef.beer@googlemail.com>
2014-12-21 10:55:26 +0000

    UPDATE - Added support for compiling using LLVMs clang

---
+++ config/gcc.mk
@@ -0,0 +1,9 @@
+# GNU gcc compiler specific config
+$(info "Using GCC")
+LN=gcc
+# PROFILER_CC_OPTIONS=-pg
+# PROFILER_LN_OPTIONS=-pg
+DEBUG_OPTIONS=-g $(PROFILER_CC_OPTIONS)
+CFLAGS_NO_ANSI=-Wstrict-prototypes --pedantic -Wall $(DEBUG_OPTIONS) -march=native
+CFLAGS= $(CFLAGS_NO_ANSI) --ansi
+LDFLAGS= $(DEBUG_OPTIONS) -lm -lrt
